985542e47349757fc7afddba894d04336eb27b17
Jose G Martinez <jgermanmm@gmail.com>
2023-02-01 00:12:43 -0300

    Propuesta modelo DB Wine

--- api/src/models/Wine.js
+++ api/src/models/Wine.js
@@ -3,9 +3,49 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
 
   sequelize.define('wine', {
+    id : {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    alcohol: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    volume: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+
   });
 };
