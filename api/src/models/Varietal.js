4ab4257e79ca24dd3f4cb883ca66af211be7ef69
Jose G Martinez <jgermanmm@gmail.com>
2023-02-01 11:21:31 -0300

    Ajuste models

--- api/src/models/Varietal.js
+++ api/src/models/Varietal.js
@@ -2,15 +2,11 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('varietal', {
-        id : {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true,
-          },
-        varietal_name: {
+        name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+    },{
+        timestamps: false,
     });
 };
