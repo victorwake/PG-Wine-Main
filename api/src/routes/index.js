810393d51289b3134d1c8c86a062e422718c9a1d
Jose G Martinez <jgermanmm@gmail.com>
2023-02-01 15:51:52 -0300

    Creación de funcionalidad para buscar vino detail

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const generalRouter = require('./generalRouter.js');
 const varietalRouter = require('./varietalRouter.js');
+const detailRouter = require('./detailRouter.js');
 
 
 
@@ -8,6 +9,7 @@ const router = Router();
 
 router.use('/general', generalRouter);
 router.use('/varietal', varietalRouter);
+router.use('/', detailRouter);
 
 
 module.exports = router;
