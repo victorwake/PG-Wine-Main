1572893dd208b7b3ce72d818b7cc0ab1927f5d68
Lucho2590 <lopezlucianomartin@gmail.com>
2023-02-02 19:50:23 -0300

    navBar

--- client/src/components/general/General.jsx
+++ client/src/components/general/General.jsx
@@ -1,5 +1,6 @@
 import './general.css';
 import { SearchBar } from '../searchBar/SearchBar';
+import { NavBar } from '../navBar/NavBar';
 import {useDispatch, useSelector} from 'react-redux';
 
 
@@ -10,71 +11,16 @@ export const General = () => {
     const dispatch = useDispatch();
     // const allVarietal = useSelector(state => state.varietal)
 
-    function handleClick(e){
-        e.preventDefault();
-        // dispatch(cleanAllFilters());
-        }
+    // function handleClick(e){
+    //     e.preventDefault();
+    //     // dispatch(cleanAllFilters());
+    //     }
         
 
     return (
         <div className='general'>
             <div className='general_nav'>
-                    <h1>Esta es la Nav</h1><div className="container_nav">
-                <div className="filters">
-                    <div className="dietsFilter">
-                    <label>Varietal: </label>
-                        <select>
-                   {/* {allVarietal.map(d => (
-                                 <option value={d.name}>{d.name}</option>
-                            ))}  */}
-                        </select>
-                    </div>
-                    <div>
-                        <label>Tipo: </label> 
-                        <select>
-                        {/* {allTipes.map(d => (
-                                 <option value={d.name}>{d.name}</option>
-                            ))} */}
-                        </select>
-                    </div>
-                    <div>
-                        <label>Region: </label> 
-                        <select>
-                        {/* {allDiets.map(d => (
-                                 <option value={d.name}>{d.name}</option>
-                            ))} */}
-                        </select>
-                    </div>
-                    <div>
-                        <label>Bodega: </label> 
-                        <select>
-                        {/* {allDiets.map(d => (
-                                 <option value={d.name}>{d.name}</option>
-                            ))} */}
-                        </select>
-                    </div>
-
-                    <div className="orderName">
-                        <label>Ordenar por Nombre:  </label>
-                        <select >
-                            <option value= 'asc'>A-Z</option>
-                            <option value= 'desc'>Z-A</option>
-                        </select>
-                    </div>
-                    <div className="orderPrice">
-                        <label> Precio: </label>
-                        <select  >
-                            <option value= 'asc'>Mayor Precio</option>
-                            <option value= 'desc'>Menor Precio</option>
-                        </select>
-                    </div>
-                    <button className="refreshButton" onClick={ e => {handleClick (e)}}>Clean filter</button>
-                </div>
-                <div className="serchBar">
-                    <SearchBar/>
-                </div>
-            </div>
-
+                <NavBar/>
             </div>
             <div className='general_data'>
                 <h1>aca van las card</h1>
--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,4 +1,70 @@
+import { SearchBar } from '../searchBar/SearchBar';
 import './navBar.css';
 
 export const NavBar = () => {
+
+    function handleClick(e){
+        e.preventDefault();
+        // dispatch(cleanAllFilters());
+        }
+
+    return(
+        <div className='general_nav'>
+            <div className='menu_bar'>
+                <a href='#' className='bt_menu'><span class='icon_list'/>Menu</a>
+            </div>
+                <div className='general_filter' >
+                    <div className='filter'>
+                        <label>Varietal: </label>
+                        <select>
+                            {/* {allVarietal.map(d => (
+                                <option value={d.name}>{d.name}</option>
+                            ))}  */}     
+                         </select>
+                    </div>
+                    <div className='filter'>
+                        <label>Tipo: </label> 
+                        <select>
+                            {/* {allTipes.map(d => (
+                                    <option value={d.name}>{d.name}</option>
+                                ))} */}
+                        </select>
+                    </div>
+                    <div className='filter'>
+                        <label>Region: </label> 
+                        <select>
+                        {/* {allDiets.map(d => (
+                                <option value={d.name}>{d.name}</option>                                
+                                ))} */}
+                        </select>
+                    </div>
+                    <div className='filter'>
+                        <label>Bodega: </label> 
+                        <select>
+                        {/* {allDiets.map(d => (
+                                <option value={d.name}>{d.name}</option>
+                            ))} */}
+                        </select>
+                    </div >
+                    <div className="order">
+                        <label>Ordenar por Nombre:  </label>
+                        <select >
+                            <option value= 'asc'>A-Z</option>
+                            <option value= 'desc'>Z-A</option>
+                        </select>
+                    </div>
+                    <div className="order">
+                        <label> Precio: </label>
+                        <select  >
+                            <option value= 'asc'>Mayor Precio</option>
+                            <option value= 'desc'>Menor Precio</option>
+                        </select>
+                    </div>
+                    <button className="refreshButton" onClick={ e => {handleClick (e)}}>Clean filter</button>
+                </div>
+                <div className="serchBar">
+                    <SearchBar/>
+                </div>
+            </div>
+    )
 }
