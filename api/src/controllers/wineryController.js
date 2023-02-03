7cd37d2a2b8d9ecce6927d4cacfa9125be63a704
Jose G Martinez <jgermanmm@gmail.com>
2023-02-03 09:58:03 -0300

    Incorporación de ruta filtrado por winery (params)

---
+++ api/src/controllers/wineryController.js
@@ -0,0 +1,35 @@
+const { Wine } = require("../db.js");
+
+const getWinery = async (req, res) => {
+  let wineryParams = req.params.winery.toLowerCase().replace(/ /g, "");
+  let wines = await Wine.findAll();
+  let wineryReplace = await wines.map((el) =>
+    el.winery.toLowerCase().replace(/ /g, "")
+  );
+
+  let wineryExits = await wineryReplace.some((el) => el === wineryParams);
+  
+  if (!wineryExits) {
+    return res.status(401).json({
+      msg: `Acción no permitida, indica una winery que corresponda`,
+    });
+  } else {
+    let wineryFind = await wines.filter(
+      (el) => el.winery.replace(/ /g, "").toLowerCase() === wineryParams
+    );
+    console.log(wineryFind);
+
+    try {
+      wineryFind.length
+        ? res.status(200).send(wineryFind)
+        : res.status(400).json({
+            msg: "Acción no permitida, indica una winery que corresponda. 002",
+          });
+      console.log(wineryFind);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+};
+
+module.exports = { getWinery };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,9 +3,8 @@ const generalRouter = require('./generalRouter.js');
 const varietalRouter = require('./varietalRouter.js');
 const detailRouter = require('./detailRouter.js');
 const colourTypeRouter = require('./colourTypeRouter.js');
+const wineryRouter = require('./wineryRouter.js');
 const login = require('./userLogin.js')
-
-
 
 const router = Router();
 
@@ -13,6 +12,7 @@ router.use('/general', generalRouter);
 router.use('/varietal', varietalRouter);
 router.use('/winedetail', detailRouter);
 router.use('/colourtype', colourTypeRouter);
+router.use('/winery', wineryRouter);
 router.use('/auth', login);
 
 
