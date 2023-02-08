6d28726e8293eafe0b280cdebbebaf39564130ce
Jose G Martinez <jgermanmm@gmail.com>
2023-02-08 18:01:49 -0300

    Creación, validación y protección de rutas con JWT

--- api/src/controllers/usuarios/userLogin.js
+++ api/src/controllers/usuarios/userLogin.js
@@ -1,10 +1,57 @@
-const {response} = require('express')
+const bcryptjs = require('bcryptjs')
 
-const login = (req, res = response ) => {
+const {
+    User
+} = require("../../db.js");
 
-    res.json({
-        msg: 'Login ok'
-    })
+const { generarJWT } = require('../../helpers/generar-jwt');
+
+
+
+const login = async(req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+      
+        // Verificar si el email existe
+        const usuario = await User.findOne({ where: { email: email } });
+        if ( !usuario ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+
+        // SI el usuario está activo
+        if ( usuario.status !== "active" ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            });
+        }
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, usuario.password );
+        if ( !validPassword ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.idUser );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }   
+
 }
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,13 +7,16 @@ const orderByPriceAscRouter = require('./orderByPriceAscRouter')
 const createWineRouter = require('./createWineRouter');
 const updateWineRouter = require('./updateWineRouter');
 const usuariosRouter = require('./usuariosRouter');
-const login = require('./userLogin.js')
+const login = require('./userLogin')
+const rutaRestringida = require('./rutaRestringida')
+
+const { validarJWT } = require('../../src/middlewares/validarJWT');
 
 
 
 const router = Router();
 
-router.use('/home', generalRouter);
+router.use('/home',  [validarJWT], generalRouter);
 router.use('/varietal', varietalRouter);
 router.use('/details', detailRouter);
 router.use('/vinos', colorTypeRouter);
@@ -21,7 +24,8 @@ router.use('/auth', login);
 router.use('/', orderByPriceAscRouter);
 router.use('/', createWineRouter);
 router.use('/', updateWineRouter);
-router.use('/usuarios', usuariosRouter)
+router.use('/usuarios', usuariosRouter);
+router.use('/rutarestringida',[validarJWT], rutaRestringida)
 
 
 module.exports = router;
