0e1ae0fb5ba26125139cb1e61ed7406fa143cea4
Jose G Martinez <jgermanmm@gmail.com>
2023-02-08 13:34:06 -0300

    Incluí try cach en usuarioscontroller

--- api/src/controllers/usuarios/usuariosController.js
+++ api/src/controllers/usuarios/usuariosController.js
@@ -1,14 +1,34 @@
-const { User } = require("../../db.js");
+const {
+    User
+} = require("../../db.js");
 const bcryptjs = require('bcryptjs');
 
-const usuariosPost = async(req, res ) => {
-    
-    const { userName, email, firstName, lastName, cellphone, profilePic, password} = req.body;
-    const usuario = await User.create({ userName:userName, email:email, firstName:firstName, lastName:lastName, cellphone:cellphone, profilePic:profilePic, password:password });
+const usuariosPost = async (req, res) => {
+
+    const {
+        userName,
+        email,
+        firstName,
+        lastName,
+        cellphone,
+        profilePic,
+        password
+    } = req.body;
+
+    try {
+    const usuario = await User.create({
+        userName: userName,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        cellphone: cellphone,
+        profilePic: profilePic,
+        password: password
+    });
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync( password, salt );
+    usuario.password = bcryptjs.hashSync(password, salt);
 
     // Guardar en BD
     await usuario.save();
@@ -16,7 +36,13 @@ const usuariosPost = async(req, res ) => {
     res.json({
         usuario
     });
+    console.log(usuario)
+
+} catch (error) {
+        console.log(error)
+
+    }
 }
 module.exports = {
-        usuariosPost
-    }
+    usuariosPost
+}
