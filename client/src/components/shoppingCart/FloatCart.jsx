2c186cc7b9989885ff1335fe3d093d65564e87a3
Eduardocid200 <eduardocid1986@gmail.com>
2023-02-16 21:46:07 -0300

    aun no

--- client/src/components/shoppingCart/FloatCart.jsx
+++ client/src/components/shoppingCart/FloatCart.jsx
@@ -1,103 +1,107 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import {useDispatch, useSelector} from 'react-redux';
 import { Link } from "react-router-dom";
-import { removeFromCart, addToCart, updateCartItem } from "../../redux/actions";
+import { removeFromCart, addToCart,updateCartItem } from "../../redux/actions";
+import { Fragment, useState, useEffect } from 'react';
 
-const FloatCart = () => {
-  const cart = useSelector((state) => state.cart);
-  const totalPrice = cart.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0
-  );
-  const dispatch = useDispatch();
-  const [quantity, setQuantity] = useState(1);
 
-  const handleQuantityChange = (itemId, event) => {
-    const newQuantity = Number(event.target.value);
-    if (newQuantity > 0) {
-      setQuantity(newQuantity);
-      dispatch(updateCartItem(itemId, newQuantity));
+export const FloatCart = ({ image, name, color_type, varietal , winery, price, id }) => {//se lo paso por props
+   
+const clase = useSelector(store => store.theme);
+const [quantity, setQuantity] = useState(1);
+const totalPrice = cart.reduce((total, item) => total + (item.price * item.quantity), 0);
+    
+    const handleQuantityChange = (event) => {
+        setQuantity(event.target.value);
     }
-  };
 
-  const handleAddToCart = (itemId) => {
-    const existingItem = cart.find((item) => item.id === itemId);
-    if (existingItem) {
-      const updatedQuantity = existingItem.quantity + quantity;
-      const updatedTotalPrice = existingItem.price * updatedQuantity;
-      dispatch(updateCartItem(itemId, updatedQuantity, updatedTotalPrice));
-    } else {
-      const totalPrice = item.price * quantity;
-      dispatch(addToCart(itemId, quantity, totalPrice));
+    const handleAddToCart = () => {
+        const existingItem = cart.find(item => item.id === id);
+        if (existingItem) {
+            const updatedQuantity = existingItem.quantity + quantity;
+            const updatedTotalPrice = existingItem.price * updatedQuantity;
+            dispatch(updateCartItem(id, updatedQuantity, updatedTotalPrice));
+        } else {
+            const totalPrice = price * quantity;
+            dispatch(addToCart(id, quantity, totalPrice));
+        }
     }
-    setQuantity(1);
-  };
+    const dispatch = useDispatch();
+    document.addEventListener('DOMContentLoaded', function() {
+    var openCartModal = document.querySelector('#openCartModal');
+    var cartModal = document.querySelector('#cartModal');
+    
+    openCartModal.addEventListener('click', function() {
+      cartModal.classList.add('show');
+      cartModal.style.display = 'block';
+      document.body.classList.add('modal-open');
+    });
+  });
+  const cart = useSelector(state => state.cart)
 
-  return (
-    <div className="modal" id="openCartModal">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header border-bottom-0">
-            <h5 className="modal-title" id="exampleModalLabel">
-              Tu carrito de compras
-            </h5>
-            <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <table className="table table-image">
-              <thead>
-                <tr>
-                  <th scope="col">Imagen</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Precio</th>
-                  <th scope="col">Cantidad</th>
-                  <th scope="col">Total</th>
-                  <th scope="col">Borrar Item</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.map((item) => (
-                  <tr key={item.id}>
-                    <td>
-                      <img
-                        src={item.image}
-                        className="img-fluid img-thumbnail"
-                        alt="Vino"
-                      />
-                    </td>
-                    <td>{item.name}</td>
-                    <td>${item.price}</td>
-                    <td>
-                      <input
-                        type="number"
-                        id={`quantity-${item.id}`}
-                        name={`quantity-${item.id}`}
-                        min="1"
-                        value={item.quantity}
-                        onChange={(e) => handleQuantityChange(item.id, e)}
-                      />
-                    </td>
-                    <td>
-                      <h4 className="price-card-">{item.price * item.quantity} $</h4>
-                    </td>
-                    <td>
-                      <button onClick={() => dispatch(removeFromCart(item.id))}>X</button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="d-flex justify-content-end">
-            <div>Total Price: {totalPrice} $</div>
-          </div>
+return(
+
+<div class="modal" id="openCartModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <h5 class="modal-title" id="exampleModalLabel">
+          Tu carrito de compras
+        </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+  <table class="table table-image">
+    <thead>
+      <tr>
+        <th scope="col">Imagen</th>
+        <th scope="col">Nombre</th>
+        {/* <th scope="col">Variedad</th> */}
+        {/* <th scope="col">Bodega</th> */}
+        <th scope="col">Precio</th>
+        <th scope="col">Cantidad</th>
+        <th scope="col">Total</th>
+        <th scope='col'>Borrar Item</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+    {cart?.map((item) => (
+        <tr>
+          <td><img src={item.image} class="img-fluid img-thumbnail" alt="Vino"/></td>
+          <td>{item.name}</td>
+          {/* <td>{item.varietal}</td> */}
+          {/* <td>{item.winery}</td> */}
+          <td>${item.price}</td>
+          
+          <td>     <input
+                className="myinput"
+                type="number"
+                id={`quantity-${item.id}`}
+                name={`quantity-${item.id}`}
+                min="1"
+                value={item.quantity}
+                onChange={(e) => handleQuantityChange(e, item.id)}
+            
+            /></td>
+          <td>  <button onClick={() => dispatch(removeFromCart(item.id)) }>X</button></td>
+         <td>   <h4 className="price-card-">{item.price * item.quantity} $</h4></td>
+         <div>Total Price: {totalPrice } $</div>
+        {/* <td>  <button onClick={handleAddToCart}>Agregar al Carrito</button></td> */}
+               
+        </tr>
+))}
+      <tr>
+
+      </tr>
+    </tbody>
+  </table>
+</div>
+        <div class="d-flex justify-content-end">
+          {/* <h5>Total: <span class="price text-success">$89</span></h5> */}
+        </div>
         <div class="modal-footer border-top-0 d-flex justify-content-between">
       <Link to={'/carrito'}>
           <button  type="button">Ir al carrito</button>
