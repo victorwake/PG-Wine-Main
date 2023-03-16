e082a527d55ad659c0a783abf970c5256d376a90
Victor Pinto <victorpintowake@gmail.com>
2023-03-16 19:53:56 -0300

    landingPage arreglado

--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './landingPage.css';
-import Logo from "../landingPage/Logo.mp4"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './landingPage.css'
 
 export const LandingPage = () => {
-    return (
-     <div className="fondo">
-     <div class="main">
-     
-     <div class="d2"></div>
-     <Link to='/home'> <div class="d3">Dionisio Wines</div></Link>
-      <div class="d4"></div>
-  </div>
-  </div>
-    );
+  return (
+    <div className="fondo">
+      <div class="main">
+        <div class="d2"></div>
+        <Link to="/home">
+          {' '}
+          <div class="d3">Dionisio Wines</div>
+        </Link>
+        <div class="d4"></div>
+      </div>
+    </div>
+  )
 }
