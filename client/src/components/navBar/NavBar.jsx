09352ce9d8e56b0272f88fde16d6f66593b23a5c
Victor Pinto <victorpintowake@gmail.com>
2023-03-20 11:26:01 -0300

    navbar corregido u dark faltante

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -228,10 +228,6 @@ export const NavBar = () => {
           <div className="search-navbar">
             <SearchBar />
           </div>
-          {/* <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
         </div>
       </div>
     </nav>
