8fc98145d87ddb0a845bf83f3a9f344b8aae3ce2
victor pinto <victorpintowake@gmail.com>
2023-10-07 19:20:59 -0300

    Update generalControllers.js

--- api/src/controllers/generalControllers.js
+++ api/src/controllers/generalControllers.js
@@ -1,29 +1,32 @@
 const { Wine } = require("../db.js");
+const winesJson = require("../../data/winesDb.json");
 
 const getDbWines = async (req, res, next) => {
   try {
-    const wines = await Wine.findAll();
     const { name } = req.query;
+    
     if (name) {
       let filteredWines;
       if (name === "winery") {
-        filteredWines = wines.filter((el) =>
+        filteredWines = winesJson.filter((el) =>
           el.winery.toLowerCase().includes(name.toLowerCase())
         );
       } else {
-        filteredWines = wines.filter((el) =>
+        filteredWines = winesJson.filter((el) =>
           el.name.toLowerCase().includes(name.toLowerCase())
         );
       }
-      filteredWines.length
-        ? res.status(200).send(filteredWines)
-        : res.status(400).json({ msg: "No hay resultados para tu busqueda" });
+      if (filteredWines.length > 0) {
+        res.status(200).send(filteredWines);
+      } else {
+        res.status(400).json({ msg: "No hay resultados para tu búsqueda" });
+      }
     } else {
-      res.status(200).send(wines);
+      res.status(200).send(winesJson);
     }
   } catch (err) {
-    res.status(400).json({ error: err.msg });
+    res.status(400).json({ error: err.message });
   }
 };
-
+  
 module.exports = { getDbWines };
